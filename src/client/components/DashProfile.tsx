858fa18f8dc40290cd58d65fa393ae33b899b454
Gabriel Samoylov <gsamo122@gmail.com>
2024-06-26 04:49:56 +0300

    creating the ui for profile section

--- src/client/components/DashProfile.tsx
+++ src/client/components/DashProfile.tsx
@@ -1,7 +1,55 @@
 import React from "react";
+import { useAppSelector } from "../redux/hooks";
+import { Button, FloatingLabel } from "flowbite-react";
 
 function DashProfile() {
-  return <div>DashProfile</div>;
+  const { currentUser } = useAppSelector((state) => state.user);
+  return (
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <div className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+          <img
+            src={currentUser?.profilePicture}
+            alt="user"
+            className={`rounded-full w-full h-full object-cover border-8 border-[lightgray]`}
+          />
+        </div>
+        <FloatingLabel
+          type="text"
+          id="username"
+          label="Username"
+          variant="filled"
+          defaultValue={currentUser?.username}
+        />
+        <FloatingLabel
+          type="email"
+          id="email"
+          label="Email"
+          variant="filled"
+          defaultValue={currentUser?.email}
+        />
+        <FloatingLabel
+          type="password"
+          id="password"
+          label="Password"
+          variant="filled"
+        />
+        <Button
+          type="button"
+          className="w-full"
+          gradientDuoTone="purpleToBlue"
+          outline
+        >
+          Update
+        </Button>
+      </form>
+      <div className="text-red-500 mt-5 flex justify-between">
+        <span className="cursor-pointer">Delete User</span>
+        <span className="cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
 }
 
 export default DashProfile;
