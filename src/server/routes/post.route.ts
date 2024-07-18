a3cfdb63dbc709da7bd0280829fdbf6399159f07
Gabriel Samoylov <gsamo122@gmail.com>
2024-07-18 22:41:49 +0300

    created api route to get all posts

--- src/server/routes/post.route.ts
+++ src/server/routes/post.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { createPost } from "../controllers/post.controller";
+import { createPost, getAllPosts } from "../controllers/post.controller";
 import { verifyToken } from "../utils/verifyToken";
 
 const postRouter = express.Router();
 
 postRouter.post("/create", verifyToken, createPost);
+postRouter.get("/allPosts", getAllPosts);
 
 export default postRouter;
